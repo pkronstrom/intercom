550391845c16816fe2259885f3938500a08d6dc6
Matti Ahinko <matti.ahinko@gofore.com>
2017-01-14 18:20:44 +0200

    Use config

--- intercom.py
+++ intercom.py
@@ -8,7 +8,7 @@ class InterCom:
     def __init__(self):
         config = configparser.ConfigParser()
         config.read('intercom.ini')
-        self.mumble_client = MumbleClient(config.get('mumbleclient'))
+        self.mumble_client = MumbleClient(config['mumbleclient'])
         self.exit = False
 
     def run(self):
