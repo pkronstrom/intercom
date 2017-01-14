5b2046e356dfb5149551a120076d4cd5ce340994
Matti Ahinko <matti.ahinko@gofore.com>
2017-01-14 16:09:38 +0200

    Remove exit

--- client.py
+++ client.py
@@ -12,8 +12,6 @@ class MumbleClient:
         self.mumble.channels.find_by_name(channel).move_in()
         # mumble.set_bandwidth(200000)
 
-        self.exit = False
-
     def handle_loop(self):
         # do audio stuff
         sd = "sd"
