8c8162a89bd976a851bb7074b7279007508d601f
Matti Ahinko <matti.ahinko@gofore.com>
2017-01-14 16:00:09 +0200

    Add intercom class

---
+++ intercom.py
@@ -0,0 +1,20 @@
+import configparser
+import time
+
+from client import MumbleClient
+
+class InterCom:
+
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('intercom.ini')
+        self.mumble_client = MumbleClient()
+        self.exit = False
+
+    def run(self):
+        while not self.exit:
+            self.mumble_client.handle_loop()
+            time.sleep(0.01)
+
+if __name__ == '__main__':
+    InterCom().run()
